58637c6cc28b3578d7bd72902895fa3f9c1300aa
Michelle <michellecv+github@gmail.com>
2016-03-23 16:44:33 -0600

    Add project #1 files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+//Week 1 - Project 1
+
+
+// Log all answers to the console and write them to document
+// Please console.log("Question#") followed by console.log(yourAnswer) after each question.
+// =======================================================
+
+//1. Log your name to the console (as a string).
+
+console.log("Question 1");
+console.log("Michelle");
+
+//2. Declare a variable called 'name', and then log it to the console.
+console.log("Question 2");
+var name = "Michelle";
+console.log(name);
+
+//3. Declare two variables with number values. 
+//   Add, subtract, multiply and divide them.
+console.log("Question 3");
+var x = 3;
+var y = 22;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+//4. Declare two variables with string values. 
+//   Create a third variable that concatenates them.
+console.log("Question 4");
+var r = "cars ";
+var s = "are cool";
+var t = (r + s);
+console.log(t);
+
+//5. Add, subract, multiply and divide the strings '5' and '3'. 
+//   Observe the results and know how to explain the results if asked.
+console.log("Question 5");
+console.log("5" + "3");
+console.log("5" - "3");
+console.log("5" * "3");
+console.log("5" / "3");
+
+//6. Create a prompt that asks a user to pick a number. 
+//   Then double the answer.
+console.log("Question 6");
+var num = prompt("Pick a number", "");
+console.log(num * 2);
+
+//7. Create a custom alert.
+console.log("Question 7");
+alert("Howdy Partner");
+console.log("Howdy Partner");
+
+//8. Declare three variables called length, width and height. 
+//   Calculate area and volume.
+console.log("Question 8");
+var length = 100;
+var width = 50;
+var height = 45;
+console.log("Area = " + (length * width));
+console.log("Volume = " + (length * width * height));
+
+//9. Ask for a user's name, age and city. 
+//   In a single command display/log the sentence:
+//   "My name is...I'm....years old, and I live in....."
+console.log("Question 9");
+var name = prompt("What's your name?", "");
+var age = prompt("How old are you?", "");
+var city = prompt("Where do you live?", "");
+console.log("My name is " + name + ", I'm " + age + " years old, and I live in " + city);
+
+//10. Create an alert that uses the response from a prompt.
+console.log("Question 10");
+alert("Hi " + name);
+
